800eedcf489294b7e7f8f01854a3317e10fd02fb
Karton <116901073+Kartonowy@users.noreply.github.com>
2023-05-06 16:12:52 +0200

    Moved Login component to separate dir

--- client/src/components/loginComponents/LogInComponent.tsx
+++ client/src/components/loginComponents/LogInComponent.tsx
@@ -1,3 +1,54 @@
-function LogInComponent() {
-    return(<div></div>)
+import axios from "axios";
+
+function LogInComponent({setRegisterFormVisibility} : any) {
+
+    async function handleLogIn(e : React.FormEvent) {
+        e.preventDefault();
+
+        const form : any = e.target;
+        const user = {
+            username: form[0].value,
+            password: form[1].value
+        }
+
+        await axios.post("/login", {
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(user)
+            }
+        )
+            .then((res : any) => res.json())
+            .then(data => {
+                localStorage.setItem("token", data.token);
+            })
+    }
+
+    return(
+        <form onSubmit={e => handleLogIn(e)}>
+            <label>
+                Name:
+                <div className="InputContainer">
+                    <input type="text" name="name" />
+                </div>
+            </label>
+            <label>
+                Password:
+                <div className="InputContainer">
+                    <input type="password" name="password" />
+                </div>
+            </label>
+            <div className="Options">
+                <div className="Switch">
+                    <span className="RegisterSwitch" onClick={() => setRegisterFormVisibility(true)}>Register</span>
+                    <span className="ForgotPassword">Forgot Password?</span>
+                </div>
+                <div className="SubmitContainer">
+                    <input type="submit" value="Login" className="SubmitLoginForm"/>
+                </div>
+            </div>
+        </form>
+    )
 }
+
+export default LogInComponent
--- client/src/components/loginFormComponent.tsx
+++ client/src/components/loginFormComponent.tsx
@@ -1,32 +1,9 @@
 import {useState} from "react";
 import "../scss/loginForm.scss"
-import axios from "axios";
 import RegisterComponent from "./loginComponents/RegisterComponent"
-
+import LogInComponent from "./loginComponents/LogInComponent";
 function LoginFormComponent() {
     const [registerTab, setRegisterTab] = useState(false)
-
-    async function handleLogIn(e : React.FormEvent) {
-            e.preventDefault();
-
-            const form : any = e.target;
-            const user = {
-                username: form[0].value,
-                password: form[1].value
-            }
-
-            await axios.post("/login", {
-                headers: {
-                    "Content-Type": "application/json"
-                },
-                body: JSON.stringify(user)
-                }
-            )
-                .then((res : any) => res.json())
-                .then(data => {
-                    localStorage.setItem("token", data.token);
-                })
-    }
 
     function setRegisterTabVisibility(value : boolean) {
         setRegisterTab(value)
@@ -44,29 +21,7 @@ function LoginFormComponent() {
         return (
             <div className="LoginContainer">
                 <div className="formContainer">
-                    <form onSubmit={e => handleLogIn(e)}>
-                        <label>
-                            Name:
-                            <div className="InputContainer">
-                                <input type="text" name="name" />
-                            </div>
-                        </label>
-                        <label>
-                            Password:
-                            <div className="InputContainer">
-                                <input type="password" name="password" />
-                            </div>
-                        </label>
-                        <div className="Options">
-                            <div className="Switch">
-                                <span className="RegisterSwitch" onClick={() => setRegisterTab(true)}>Register</span>
-                                <span className="ForgotPassword">Forgot Password?</span>
-                            </div>
-                            <div className="SubmitContainer">
-                                <input type="submit" value="Login" className="SubmitLoginForm"/>
-                            </div>
-                        </div>
-                    </form>
+                    <LogInComponent setRegisterFormVisibility={setRegisterTabVisibility}></LogInComponent>
                 </div>
             </div>
         )
